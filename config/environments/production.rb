d10d3e3a817872b8e8944a2bd9c9fc0f875e5ac4
Duoduo789 <zengzhi6222@126.com>
2014-08-21 16:15:30 +0800

    devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  #note to set your actual host
+  config.action_mailer.default_url_options = { :host => 'http://duoduo789.herokuapp.com/' }
+
 end
