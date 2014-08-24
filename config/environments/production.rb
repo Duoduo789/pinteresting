ef9d8c91c7889d24ffe43b9d5d195b9c6d02d75c
Duoduo789 <zengzhi6222@126.com>
2014-08-24 21:06:30 +0800

    add two more pictures

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,13 @@ Pinteresting::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   #note to set your actual host
   config.action_mailer.default_url_options = { :host => 'http://duoduo789.herokuapp.com/' }
-
+  #sets paperclip to upload image to amazon s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket =>  ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
